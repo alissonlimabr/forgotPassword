3205e65fac74584357b6ae2af76b9b05e1322b77
Alisson Lima <amlxd5@gmail.com>
2023-03-15 14:25:33 -0500

    Projeto base da API

---
+++ reset-password-api/src/main/java/com/alissonlimabr/resetpasswordapi/ResetPasswordApiApplication.java
@@ -0,0 +1,13 @@
+package com.alissonlimabr.resetpasswordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResetPasswordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResetPasswordApiApplication.class, args);
+	}
+
+}
---
+++ reset-password-api/src/test/java/com/alissonlimabr/resetpasswordapi/ResetPasswordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonlimabr.resetpasswordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResetPasswordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
