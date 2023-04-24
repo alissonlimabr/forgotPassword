629176a9cd6cddf46c05abd6c390e2db62aab1c6
Alisson Lima <amlxd5@gmail.com>
2023-04-24 00:45:08 -0500

    Atualizações do module principal

--- reset-password-angular/src/app/app-routing.module.ts
+++ reset-password-angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SendEmailFormComponent } from './components/send-email-form/send-email-form.component';
+import { ResetPasswordFormComponent } from './components/reset-password-form/reset-password-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SendEmailFormComponent },
+  { path: 'reset', component: ResetPasswordFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- reset-password-angular/src/app/app.module.ts
+++ reset-password-angular/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResetPasswordFormComponent } from './components/reset-password-form/reset-password-form.component';
+import { SendEmailFormComponent } from './components/send-email-form/send-email-form.component';
+import { CheckEmailDirective } from './directives/check-email.directive';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResetPasswordFormComponent,
+    SendEmailFormComponent,
+    CheckEmailDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
