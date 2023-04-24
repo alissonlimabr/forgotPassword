1d46296eeb82a3154bd14fab70ad5525b7758d18
Alisson Lima <amlxd5@gmail.com>
2023-04-24 00:38:05 -0500

    Adiciona FiltroCors

---
+++ reset-password-api/src/main/java/com/alissonlimabr/resetpasswordapi/config/FilterCors.java
@@ -0,0 +1,30 @@
+package com.alissonlimabr.resetpasswordapi.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class FilterCors {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration configCors = new CorsConfiguration();
+        configCors.setAllowCredentials(true);
+        configCors.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://localhost:4200"));
+        configCors.setAllowedMethods(Arrays.asList("*"));
+        configCors.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", configCors);
+
+        return new CorsFilter(configSource);
+
+    }
+
+}
