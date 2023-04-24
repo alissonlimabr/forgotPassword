3ba6ea3538dc4b401f5c0114b0c5450166fa8343
Alisson Lima <amlxd5@gmail.com>
2023-04-24 01:24:32 -0500

    Atualiza environment

--- reset-password-angular/src/app/services/user.service.ts
+++ reset-password-angular/src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { User } from '../models/user';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
